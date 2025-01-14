69f53fb10da01f8850b9c01f8a22a7588c04c080
marlinps <marlinpurnamas92@gmail.com>
2025-01-14 13:50:33 +0700

    create doWhile c++

---
+++ doWhile.cpp
@@ -0,0 +1,18 @@
+#include  <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    int i = 1;
+
+    cout <<"n : ";
+    cin >> n;
+
+    do{
+        cout <<  i  << endl;
+        i++;
+
+    }while(i <= n);
+
+    return 0;
+}
